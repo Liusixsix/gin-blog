c1577ae452919be3bd232e9667361e9793c7a9c2
liuyan <450995596@qq.com>
2022-02-11 13:41:35 +0800

    first commit

---
+++ middleware/jwt.go
@@ -0,0 +1,40 @@
+package middleware
+
+import (
+	"gin-blog/pkg/e"
+	"gin-blog/pkg/util"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
+
+func JWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var code = e.SUCCESS
+		var data interface{}
+
+		token := c.GetHeader("token")
+
+		if token == "" {
+			code = e.INVALID_PARAMS
+		} else {
+			claims, err := util.ParseToken(token)
+			if err != nil {
+				code = e.ERROR_AUTH_CHECK_TOKEN_FAIL
+			} else if time.Now().Unix() > claims.ExpiresAt {
+				code = e.ERROR_AUTH_CHECK_TOKEN_TIMEOUT
+			}
+		}
+
+		if code != e.SUCCESS {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"code": code,
+				"msg":  e.GetMsg(code),
+				"data": data,
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
